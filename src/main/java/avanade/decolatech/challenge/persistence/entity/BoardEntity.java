48d406956802a3b4e65e873a491db56ebd0b52b7
gPinheirop <gabrielpedreira_@hotmail.com>
2025-04-05 21:48:36 -0300

    Start board queries development

--- src/main/java/avanade/decolatech/challenge/persistence/entity/BoardEntity.java
+++ src/main/java/avanade/decolatech/challenge/persistence/entity/BoardEntity.java
@@ -2,9 +2,13 @@ package avanade.decolatech.challenge.persistence.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class BoardEntity {
 
     private Long id;
     private String name;
+    private List<BoardColumnEntity> boardColumns = new ArrayList<>();
 }
---
+++ src/main/java/avanade/decolatech/challenge/service/BoardQueryService.java
@@ -0,0 +1,29 @@
+package avanade.decolatech.challenge.service;
+
+
+import avanade.decolatech.challenge.persistence.dao.BoardColumnDAO;
+import avanade.decolatech.challenge.persistence.dao.BoardDAO;
+import avanade.decolatech.challenge.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardQueryService {
+
+    private final Connection connection;
+
+    public Optional<BoardEntity> findById(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
+        var optional = dao.findById(id);
+        if (optional.isPresent()){
+            var entity = optional.get();
+            entity.setBoardColumns(boardColumnDAO.findByBoardId(entity.getId()));
+            return Optional.of(entity);
+        }
+        return Optional.empty();
+    }
+}
