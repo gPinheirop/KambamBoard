f343e0b587828ce6cb99823207d340358b0f0d58
gPinheirop <gabrielpedreira_@hotmail.com>
2025-04-05 21:58:15 -0300

    Finish query and add ui

--- src/main/java/avanade/decolatech/challenge/persistence/entity/BoardColumnKindEnum.java
+++ src/main/java/avanade/decolatech/challenge/persistence/entity/BoardColumnKindEnum.java
@@ -1,6 +1,14 @@
 package avanade.decolatech.challenge.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnKindEnum {
 
     INITIAL, FINAL, CANCEL, PENDING;
+
+    public static BoardColumnKindEnum findByName(final String name) {
+        return Stream.of(BoardColumnKindEnum.values())
+                .filter(b -> b.name().equals(name))
+                .findFirst().orElseThrow();
+    }
 }
---
+++ src/main/java/avanade/decolatech/challenge/ui/BoardMenu.java
@@ -0,0 +1,14 @@
+package avanade.decolatech.challenge.ui;
+
+import avanade.decolatech.challenge.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BoardMenu {
+
+    private final BoardEntity entity;
+
+    public void execute() {
+
+    }
+}
